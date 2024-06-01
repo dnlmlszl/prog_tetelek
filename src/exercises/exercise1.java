31bf029f367147b1b0e6eb0de383c114cb574c7a
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-06-01 09:22:38 +0200

    prog tetelek

--- src/exercises/exercise1.java
+++ src/exercises/exercise1.java
@@ -7,10 +7,52 @@ public class exercise1 {
     }
 
     private static void feladatok() {
-        konzolrair("Itt egy szoveg");
+        int sum = osszegzes();
+        konzolrair("Osszeg:" + sum);
     }
 
     private static void konzolrair(String text) {
         System.out.println(text);
+    }
+
+    private static int osszegzes() {
+        int[] numbers = {-3, 5, 8, 9, 11, 7};
+        int sum = 0;
+
+        for (int num: numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    private static int megszamlalas() {
+        return 0;
+    }
+
+    private static boolean eldontesEgy() {
+        return false;
+    }
+
+    private static boolean eldontesMind() {
+        return false;
+    }
+
+    private static int kivalasztas() {
+        return 0;
+    }
+    private static int kivalasztasMaximum() {
+        return 0;
+    }
+    private static int kivalasztasMinimum() {
+        return 0;
+    }
+
+    private static int kereses() {
+        return 0;
+    }
+
+
+    private static int[] masolas(int[] array) {
+        return array;
     }
 }
