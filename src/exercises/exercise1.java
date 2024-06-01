1f871a195b90365e1f57df02e9347e265bf8ff4e
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-06-01 09:05:00 +0200

    void feladatok

--- src/exercises/exercise1.java
+++ src/exercises/exercise1.java
@@ -2,6 +2,10 @@ package exercises;
 
 public class exercise1 {
     public static void main(String[] args) {
+        feladatok();
+    }
+
+    private static void feladatok() {
         System.out.println("Hello");
     }
 }
