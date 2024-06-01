95bf81c15b969d8c855a62ca74d94f11bd160d91
dnlmlszl <daniel.mlaszlo@yahoo.com>
2024-06-01 09:10:21 +0200

    void konzolrair(String)

--- src/exercises/exercise1.java
+++ src/exercises/exercise1.java
@@ -3,9 +3,14 @@ package exercises;
 public class exercise1 {
     public static void main(String[] args) {
         feladatok();
+        konzolrair("Itt egy szoveg");
     }
 
     private static void feladatok() {
         System.out.println("Hello");
+    }
+
+    private static void konzolrair(String text) {
+        System.out.println(text);
     }
 }
